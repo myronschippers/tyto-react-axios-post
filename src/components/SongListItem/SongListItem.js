56b22d01cfe347ebbb88185a75804e718ac4b536
Myron <dubtwee@gmail.com>
2019-12-06 14:25:26 -0600

    adding expandable click for more section on songs

--- src/components/SongListItem/SongListItem.js
+++ src/components/SongListItem/SongListItem.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
+import './Expand.css';
 
 class SongListItem extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            show: false,
+        }
+    }
+
+    clickExpand = (event) => {
+        this.setState({
+            show: !this.state.show,
+        })
+    }
 
     render() {
+        let showClass;
+
+        if (this.state.show) {
+            showClass = 'isExpanded';
+        } else if (!this.state.show) {
+            showClass = 'isHidden';
+        }
+
         return (
             <div>
-                <p>{this.props.song.id} - {this.props.song.artist} - {this.props.song.track}</p>
-                <button onClick={() => this.props.deleteSong(this.props.song.id)}>X</button>
+                <h3>{this.props.song.track}</h3>
+                <button className="clickable" onClick={this.clickExpand}>click for more</button>
+                <div className={showClass}>
+                    <ul>
+                        <li>Rank: {this.props.song.rank}</li>
+                        <li>Artist: {this.props.song.artist}</li>
+                    </ul>
+                    <button onClick={() => this.props.deleteSong(this.props.song.id)}>X</button>
+                </div>
             </div>
         )
     }
